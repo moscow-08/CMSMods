07dc3adb9574d3274543d4ddad503f1d91f88a09
Tyler Chee <tdchee@gmail.com>
2025-11-12 09:34:08 -0700

    Initial commit

---
+++ WrenchOMatic/WrenchOMatic/Configurator.cs
@@ -0,0 +1,42 @@
+﻿using MelonLoader;
+using System.Text.Json;
+using WrenchOMatic.Models;
+namespace WrenchOMatic
+{
+    public class Configurator
+    {
+        private static readonly string configFilename = "./settings.json";
+
+        private static MelonPreferences_Category _configCat;
+
+        public static MelonPreferences_Entry<int> _scrapMulti { get; set; }
+        public static MelonPreferences_Entry<bool> _bigBonusOn { get; set; }
+        public static MelonPreferences_Entry<int> _screwSpeedMulti { get; set; }
+        public static MelonPreferences_Entry<int> _inspectMulti { get; set; }
+        public static MelonPreferences_Entry<int> _walkSpeedMulti { get; set; }
+        public static MelonPreferences_Entry<int> _partMoveMulti { get; set; }
+
+        public Configurator()
+        {
+            _configCat = MelonPreferences.CreateCategory("PlayerSkillsMod");
+            _configCat.SetFilePath(configFilename, autoload: true);
+
+            if (!_configCat.HasEntry("ScrapMultiplier"))
+            {
+                _scrapMulti = _configCat.CreateEntry("ScrapMultiplier", 1, null, null, is_hidden: false, dont_save_default: false, null);
+            }
+            else
+            {
+                _scrapMulti = _configCat.GetEntry<int>("ScrapMultiplier");
+            }
+        }
+
+        public static void ExtractConfigValues(MelonPreferences_Category settings)
+        {
+            
+        }
+        public static PlayerSettings MapSettingsConfig(MelonPreferences_Category playerSettings)
+        {
+        }
+    }
+}
---
+++ WrenchOMatic/WrenchOMatic/Models/PlayerSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrenchOMatic.Models
+{
+    public class PlayerSettings
+    {
+        public double ScrapMultiplier;
+        public bool AlwaysBigBonus;
+        public double ScrewSpeedMultiplier;
+        public double InspectMultiplier;
+        public double WalkingSpeedMultiplier;
+        public double PartMovementMultiplier;
+
+        public PlayerSettings()
+        {
+            ScrapMultiplier = 1;
+            AlwaysBigBonus = false;
+            ScrewSpeedMultiplier = 1;
+            InspectMultiplier = 1;
+            WalkingSpeedMultiplier = 1;
+            PartMovementMultiplier = 1;
+        }
+    }
+}
